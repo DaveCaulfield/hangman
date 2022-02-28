4c128a647acc1dbccb83b77b50c05ec9a67dee3a
DaveCaulfield <dave.caulfield@gmail.com>
2022-02-28 22:11:23 +0000

    Add lives feature to game

--- run.py
+++ run.py
@@ -5,7 +5,9 @@
 import random
 
 test_list = ["avenue", "awkward", "diagram"]
-
+lives = 6
+game_over = False
+list_blanks = []
 
 def generate_random_word():
     """
@@ -15,43 +17,15 @@ def generate_random_word():
     return random_word
 
 
-# def player_guess():
-#     """
-#     Generate a random word
-#     """
-#     player_guess = input("Pick a letter:").lower()
-
-#     try:
-#         if len(player_guess) != 1:
-#             raise ValueError(
-#                 f"only one letter allowed. you entered {len(player_guess)} letters {player_guess}"
-#             )
-#         elif not player_guess.isalpha():
-#             raise ValueError(
-#                 f"only letters allowed. you entered {player_guess}"
-#             )
-#         # add elif when user already
-
-#     except ValueError as e:
-#         print(f"invalid data - {e}")
-
-#     return player_guess
-
-
-
-
-
-# the game loop
 random_word = generate_random_word()  # call the random word generator
 print(f"for testing  - - random word is : {random_word.upper()}")
-list_blanks = []
+
 for i in range(len(random_word)):
     list_blanks += "_"
 print(list_blanks)
 
 
-
-game_over = False
+# the game loop
 while not game_over:
     
     player_guess = input("Pick a letter:").lower()
@@ -77,6 +51,13 @@ while not game_over:
             print(f"{player_guess.upper()} is a letter in the word")
             list_blanks[i] = letter.upper()
     print(list_blanks)
+
+    if player_guess not in random_word:
+        print(f"\nThats not a letter in the word - you lose a life")
+        lives -= 1
+        if lives == 0:
+            print(f"\nYou have no lives left")
+        print(f"{lives} lives left")
 
     if "_" not in list_blanks:
         game_over = True
