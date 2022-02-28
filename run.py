8c1bac60b6a8fca25248169ef2fd7384ef34f2a3
DaveCaulfield <dave.caulfield@gmail.com>
2022-02-28 12:47:42 +0000

    generate random word from test word list

--- run.py
+++ run.py
@@ -2,3 +2,9 @@
 # ask user to guess letter
 # check user input is valid
 # check if user guessed letter is inthe random word
+import random
+
+test_list = ["avenue", "awkward", "diagram"]
+
+random_word = random.choice(test_list)
+print(random_word)
