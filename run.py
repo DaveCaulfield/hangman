f4d60e9457faa9a4dfe168ed969b7f0a947bdb99
DaveCaulfield <dave.caulfield@gmail.com>
2022-02-28 12:47:06 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# generate random word
+# ask user to guess letter
+# check user input is valid
+# check if user guessed letter is inthe random word
