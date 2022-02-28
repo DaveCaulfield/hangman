9ecaffbd277944d390dfb6afa87176e8f1dee717
DaveCaulfield <dave.caulfield@gmail.com>
2022-02-28 17:29:13 +0000

    Add foor loop to print out list of blank spaces for random word

--- run.py
+++ run.py
@@ -11,17 +11,23 @@ def generate_random_word():
     Generate a random word
     """
     return random.choice(test_list)
-    
-  
-random_word = generate_random_word()
-print(random_word)
-display =[]
-for i in range(len(random_word)):
-    display += "_"
-print(display)
-
 
 def player_guess():
+    """
+    Generate a random word
+    """
     return input("Pick a letter:").lower()
 
+    
+def game_loop(): 
+    random_word = generate_random_word()
+    print(random_word)
+    list_blanks =[]
+    for i in range(len(random_word)):
+        list_blanks += "_"
+    print(list_blanks)
+
+
+
+game_loop()
 guess = player_guess()
