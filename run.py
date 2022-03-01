5ee28b35753fceaa7843704377d1176c37565af6
DaveCaulfield <dave.caulfield@gmail.com>
2022-03-01 20:55:56 +0000

    import os and add clear feature after user input

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 # check user input is valid
 # check if user guessed letter is in the random word
 import random
+import os 
 from ascii_art import hangman_pics
 from ascii_art import hangman_graphic
 
@@ -28,37 +29,38 @@ def generate_random_word():
 
 
 random_word = generate_random_word()  # call the random word generator
-print(f"for testing  - - random word is : {random_word.upper()}")
+
 
 for i in range(len(random_word)):
     list_blanks += "_"
-print(f"You have {lives} lives\n")
+print(f"Lives: {lives}\n")
 print(f"{str(' '.join(list_blanks)).upper()}\n")  # join list blanks for cleaner user experience
-  
+print(f"TESTWORD IS -->> {random_word.upper()}") 
 
 
 # the game loop
 while not game_over:
     
-    player_guess = input("Pick a letter:").lower()
+    player_guess = input("Guess a letter:").lower()
+
+    os.system('clear') 
         
 
     try:  
         if len(player_guess) != 1:
             raise ValueError(
-                f"only one letter allowed. you entered {len(player_guess)} letters {player_guess}"
+                f"only one letter allowed. you entered {len(player_guess)} characters {player_guess}"
             )
         elif not player_guess.isalpha():
             raise ValueError(
                 f"only letters allowed. you entered {player_guess}"
             )
         elif player_guess in list_blanks:
-            raise ValueError(
             print(f"You already guessed {player_guess}")
-            )
             
+
     except ValueError as e:
-        print(f"invalid data - {e}")
+        print(f"warning {e}")
     #display any correct letter guesses
     for i in range(len(random_word)):
         letter = random_word[i]
@@ -76,8 +78,11 @@ while not game_over:
             print(f"{str(' '.join(list_blanks)).upper()}\n")
 
 
+            
 
-    if player_guess not in random_word:
+
+
+    if player_guess.isalpha() and player_guess not in random_word:
         print(f"\n{player_guess.upper()} is not a letter in the word - you lose a life")
         lives -= 1
         print(hangman_pics[lives])
