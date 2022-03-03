0509bb783e504ab550065e5df7e7a869a85d15be
DaveCaulfield <dave.caulfield@gmail.com>
2022-03-03 20:54:48 +0000

    implement game display template function to prevenr re-using same code

--- run.py
+++ run.py
@@ -48,6 +48,13 @@ print(f"Lives: {lives}\n")
 print(f"{Fore.CYAN}{str(' '.join(list_blanks)).upper()}{Style.RESET_ALL}\n")  # join list blanks for cleaner user experience
 print(f"TESTWORD IS -->> {random_word.upper()}")
 
+def game_display_template():
+    print(f"{Fore.CYAN}{hangman_graphic}{Style.RESET_ALL}\n")  #combine line and roll out to other parts
+    print("      WELCOME TO THE HANGMAN WORD GAME\n")  #combine line and roll out to other parts
+    print(hangman_pics[lives])  #combine line and roll out to other parts
+    print(f"You have {lives} lives\n")  #combine line and roll out to other parts
+    print(f"{Fore.CYAN}{str(' '.join(list_blanks)).upper()}{Style.RESET_ALL}\n") 
+
 
 # Game on loop
 while not game_over:
@@ -67,6 +74,7 @@ while not game_over:
             raise ValueError(
                 f"only one letter allowed. you entered {len(player_guess)} characters '{player_guess}'"
             )
+        # guess is not a letter
         elif not player_guess.isalpha():
             print(f"{Fore.CYAN}{hangman_graphic}{Style.RESET_ALL}\n")  #combine line and roll out to other parts
             print("      WELCOME TO THE HANGMAN WORD GAME\n")  #combine line and roll out to other parts
@@ -76,6 +84,10 @@ while not game_over:
             raise ValueError(
                 f"only letters allowed. you entered '{player_guess}'"
             )
+            #if guess same letter
+        elif player_guess in list_blanks:
+            print(f"You already guessed {player_guess}")
+        #deduct life    
         elif player_guess not in random_word:
             lives -= 1
             print(f"{Fore.CYAN}{hangman_graphic}{Style.RESET_ALL}\n")  #combine line and roll out to other parts
@@ -84,9 +96,7 @@ while not game_over:
             print(f"You have {lives} lives\n")  #combine line and roll out to other parts
             print(f"{Fore.CYAN}{str(' '.join(list_blanks)).upper()}{Style.RESET_ALL}\n") 
             print(f"{player_guess.upper()} is not a letter in the word - you lose a life\n")
-        #if guess same letter
-        elif player_guess in list_blanks:
-            print(f"You already guessed {player_guess}")
+        
     #Customer error message
     except ValueError as e:
         print(f"Caution {e}\n")
@@ -96,12 +106,13 @@ while not game_over:
         letter = random_word[i]
         #Correct guesses
         if letter == player_guess:
-            print(f"{Fore.CYAN}{hangman_graphic}{Style.RESET_ALL}\n")  #combine line and roll out to other parts
-            print("      WELCOME TO THE HANGMAN WORD GAME\n")  #combine line and roll out to other parts
-            print(hangman_pics[lives])  #combine line and roll out to other parts
-            print(f"You have {lives} lives\n")  #combine line and roll out to other parts
-            list_blanks[i] = letter
-            print(f"{Fore.CYAN}{str(' '.join(list_blanks)).upper()}{Style.RESET_ALL}\n") 
+            game_display_template()
+            # print(f"{Fore.CYAN}{hangman_graphic}{Style.RESET_ALL}\n")  #combine line and roll out to other parts
+            # print("      WELCOME TO THE HANGMAN WORD GAME\n")  #combine line and roll out to other parts
+            # print(hangman_pics[lives])  #combine line and roll out to other parts
+            # print(f"You have {lives} lives\n")  #combine line and roll out to other parts
+            # list_blanks[i] = letter
+            # print(f"{Fore.CYAN}{str(' '.join(list_blanks)).upper()}{Style.RESET_ALL}\n") 
             print(f"{player_guess.upper()} is a letter in the word\n")
 
     #Game over condition
