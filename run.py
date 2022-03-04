55fc72192a4c47e2c804905f4ba6c97a99e8ff30
DaveCaulfield <dave.caulfield@gmail.com>
2022-03-04 19:56:11 +0000

    add space to start of feedback messages

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ def landing_page():
     print("        WELCOME TO THE HANGMAN WORD GAME\n")
     print("     Guess all the letters in the word to win.\n")
 
-    print("\nTo beat the Hangman:")
+    print("\n To beat the Hangman:")
     print("- Enter one letter at a time.")
     print("- If your guess is correct the letter will be displayed.")
     print("- If your guess is wrong you lose a life.")
@@ -56,18 +56,18 @@ def gamearea_display():
     os.system('clear')
     print(f"{Fore.CYAN}{hangman_graphic}{Style.RESET_ALL}")  #combine line and roll out to other parts
     # print("      WELCOME TO THE HANGMAN WORD GAME\n")  #combine line and roll out to other parts
-    print(hangman_pics[lives])  #combine line and roll out to other parts
-    print(f"You have {lives} lives\n")  #combine line and roll out to other parts
+    print(f"   hangman_pics[lives]")  #combine line and roll out to other parts
+    print(f" You have {lives} lives\n")  #combine line and roll out to other parts
     
     #if guess same letter
     if player_guess in list_blanks:
-        print(f"{Fore.YELLOW}You already guessed the letter {player_guess.upper()}{Style.RESET_ALL}\n")
+        print(f"{Fore.YELLOW} You already guessed the letter {player_guess.upper()}{Style.RESET_ALL}\n")
   
 
 # Game on loop
 while not game_over:
 
-    player_guess = input("Guess a letter:\n").lower()
+    player_guess = input(" Guess a letter:\n").lower()
 
     os.system('clear')
 
@@ -77,14 +77,14 @@ while not game_over:
             gamearea_display()
             print(f"{Fore.CYAN}{str(' '.join(list_blanks)).upper()}{Style.RESET_ALL}\n")
             raise ValueError(
-                f"{Fore.YELLOW}only one letter allowed, you entered {len(player_guess)} characters '{player_guess}'{Style.RESET_ALL}"
+                f"{Fore.YELLOW} only one letter allowed, you entered characters '{player_guess}'{Style.RESET_ALL}"
             )
         # guess is not a letter
         elif not player_guess.isalpha():
             gamearea_display()
             print(f"{Fore.CYAN}{str(' '.join(list_blanks)).upper()}{Style.RESET_ALL}\n")
             raise ValueError(
-                f"{Fore.YELLOW}only letters allowed, you entered '{player_guess}{Style.RESET_ALL}'"
+                f"{Fore.YELLOW} only letters allowed, you entered '{player_guess}'{Style.RESET_ALL}"
             )
        
         #deduct life    
@@ -96,7 +96,7 @@ while not game_over:
         
     #Customer error message
     except ValueError as e:
-        print(f"{Fore.YELLOW}Caution: {e}{Style.RESET_ALL}\n")
+        print(f"{Fore.YELLOW} Caution: {e}{Style.RESET_ALL}\n")
 
     #display any correct letter guesses
     for i in range(len(random_word)):
@@ -105,15 +105,15 @@ while not game_over:
         if letter == player_guess:
             gamearea_display()
             list_blanks[i] = letter
-            print(f"{Fore.CYAN}{str(' '.join(list_blanks)).upper()}{Style.RESET_ALL}\n") 
-            print(f"{player_guess.upper()} is a letter in the word\n")
+            print(f" {Fore.CYAN}{str(' '.join(list_blanks)).upper()}{Style.RESET_ALL}\n") 
+            print(f" {player_guess.upper()} is a letter in the word\n")
 
     #Game over condition
     if lives == 0:
-        print(f"\nYou have no lives left")
-        print(f"\nThe word was {random_word.upper()}\n")
+        print(f"\n You have no lives left")
+        print(f"\n The word was {random_word.upper()}\n")
         game_over = True
-        print(f"{Fore.RED}Game Over{Style.RESET_ALL}")
+        print(f"{Fore.RED} Game Over{Style.RESET_ALL}")
 
     #Game over condition
     if "_" not in list_blanks:
