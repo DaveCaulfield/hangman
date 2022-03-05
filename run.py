e8218005880b4ce64fe5ed5f22b1ee212aef508f
DaveCaulfield <dave.caulfield@gmail.com>
2022-03-05 16:42:28 +0000

    Add start function

--- run.py
+++ run.py
@@ -30,7 +30,7 @@ def landing_page():
 
     # print(hangman_pics[6])
 
-landing_page()
+
 
 
 # Generate random word
@@ -42,21 +42,31 @@ def generate_random_word():
     if level == "1":
         os.system('clear')
         random_word = random.choice(wordlist1)
+        return random_word
     elif level == "2":
         os.system('clear')
         random_word = random.choice(wordlist2)
+        return random_word
+    
     else:
         os.system('clear')
         landing_page()
         generate_random_word()
-        
+    
+
+def start():
+    landing_page()
+    generate_random_word()
    
-    return random_word
+start()
+
+
 
 random_word = generate_random_word()  # call the random word generator
 
 # Display list of blank letters to be filled
 for i in range(len(random_word)):
+    # print(random_word)
     list_blanks += "_"
 print(f"{Fore.CYAN}{hangman_graphic}{Style.RESET_ALL}")
 print(hangman_pics[lives])
@@ -127,12 +137,14 @@ while not game_over:
         print(f" You have no lives left ...The word was {random_word.upper()}")
         game_over = True
         print(f"{Fore.RED}                GAME OVER{Style.RESET_ALL}")
+       
 
     # Game over condition
     if "_" not in list_blanks:
         game_over = True
         print(f"\n Congratulations! the word is {random_word.upper()} - {Fore.GREEN}You win!!{Style.RESET_ALL}\n\n")
-
+        play_again = input("play again? press 1\n")
+        
 
 
 
